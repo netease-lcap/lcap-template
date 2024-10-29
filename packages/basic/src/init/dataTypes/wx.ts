b8c773214286ff86d2b68f542797d4fdb341387e
御风 <18012261618@126.com>
2024-10-29 11:28:30 +0800

    chore: 同步小程序函数

--- packages/basic/src/init/dataTypes/wx.ts
+++ packages/basic/src/init/dataTypes/wx.ts
@@ -4,7 +4,9 @@
 const getUrl = (url) => url.startsWith('http') ? url : 'http://' + url;
 
 /* 小程序环境 */
-export const isMiniApp = () => window.__wxjs_environment === 'miniprogram';
+export const isMiniApp = (() => {
+  return localStorage.getItem("_wx_is_mini") || window.__wxjs_environment === 'miniprogram'
+})()
 
 /* 路由跳转 */
 export const navigateTo = ({ url }) => {
@@ -22,4 +24,28 @@ export const navigateToUserInfoPage = () => {
         return;
     const uri = location.href;
     window.wx.miniProgram.navigateTo({ url: `/pages/userinfo/index?redirect_uri=${uri}` });
+};
+
+/* 跳转到手机号页面*/
+export const navigateToUserPhonePage = () => {
+    if (!isMiniApp)
+        return;
+    const uri = location.href;
+    window.wx.miniProgram.navigateTo({ url: `/pages/userphone/index?redirect_uri=${uri}` });
+};
+
+/* 跳转到扫一扫页面*/
+export const navigateScanCodePage = () => {
+    if (!isMiniApp)
+        return;
+    const uri = location.href;
+    window.wx.miniProgram.navigateTo({ url: `/pages/scancode/index?redirect_uri=${uri}` });
+};
+
+/* 跳转到定位页面*/
+export const navigateLocationPage = () => {
+    if (!isMiniApp)
+        return;
+    const uri = location.href;
+    window.wx.miniProgram.navigateTo({ url: `/pages/getlocation/index?redirect_uri=${uri}` });
 };
