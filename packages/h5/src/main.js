68f3e281e104c3b209f1abb551eb2c0d17b950b3
御风 <18012261618@126.com>
2024-12-17 15:53:07 +0800

    Feature/合并PC、mobile (#226)
    * chore: 整合Vue应用目录

--- packages/h5/src/main.js
+++
@@ -1,8 +0,0 @@
-import { routes } from "./router/routes";
-
-import metaData from "./metaData.js";
-import platformConfig from "./platform.config.json";
-import cloudAdminDesigner from "./init";
-import "./library";
-
-cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);
