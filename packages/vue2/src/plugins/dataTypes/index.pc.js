9e91f09dc7a907de16bd3f678ce1dd0950184e21
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2025-03-12 12:21:00 +0800

    fix: 修复Vue2 logout逻辑 (#341)
    Co-authored-by: 御风 <18012261618@126.com>

--- packages/vue2/src/plugins/dataTypes/index.pc.js
+++ packages/vue2/src/plugins/dataTypes/index.pc.js
@@ -4,12 +4,12 @@ import { cookie } from "@/common";
 export const utils = {
   logout() {
     // FIXME 从全局变量中获取
-    Vue.prototype
-      .$confirm({
-        title: "提示",
-        content: "确定退出登录吗？",
-        message: "确定退出登录吗?",
-      })
+    const $confirm = typeof Vue.prototype.$confirm === "function" ? Vue.prototype.$confirm : () => Promise.resolve();
+    $confirm({
+      title: "提示",
+      content: "确定退出登录吗？",
+      message: "确定退出登录吗?",
+    })
       .then(() => Vue.prototype.$auth.logout())
       .then(() => {
         cookie.erase("authorization");
