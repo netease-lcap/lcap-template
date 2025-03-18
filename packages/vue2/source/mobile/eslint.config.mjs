893cfe3fd973dc3b01956a62e9e2b5ec0ae832b4
mcuking <mcuking.tang@gmail.com>
2025-03-18 15:00:31 +0800

    feat: 更新 vue2 的 eslint 规则

---
+++ packages/vue2/source/mobile/eslint.config.mjs
@@ -0,0 +1,27 @@
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['src/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/packages/**', '**/common/**', '**/metaData.js'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+  {
+    rules: {
+      'vue/no-useless-template-attributes': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/valid-v-slot': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+}
+];
