7bd91c0e4f0813d176870d26104b93d863ca66ff
mcuking <mcuking.tang@gmail.com>
2025-03-18 17:08:34 +0800

    feat: 更新 react eslint 配置

---
+++ packages/react/source/mobile/eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/packages/**'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['src/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+)
