d8dd979ba31d4b47cffe44f70022e1c0fc7f4cd7
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2025-03-26 11:26:28 +0800

    fix: 注释掉getJsonParse函数以简化JSON解析逻辑 (#350) (#358)
    Co-authored-by: 不可 <512828461@qq.com>

--- packages/basic/src/utils/create/index.ts
+++ packages/basic/src/utils/create/index.ts
@@ -19,23 +19,23 @@ import { overwriteErrorMsgFieldIfSpecified } from "./utils";
 import { default as builtInInterceptors } from "./interceptors";
 
 const getData = (str) => new Function("return " + str)();
-function getJsonParse() {
-  let hasSource = false;
-  const jsonStr = `{"myBigInt":6028792033986383748 }`;
-  JSON.parse(jsonStr, (...arg) => {
-    if (get(arg, "2")) hasSource = true;
-    return arg[1];
-  });
-  const warpJsonParse = (jsonStr) =>
-    JSON.parse(jsonStr, (...arg) => {
-      if (typeof arg[1] === "number" && Number.isInteger(arg[1]) && !Number.isSafeInteger(arg[1])) {
-        return new BigNumber(get(arg, "2.source"));
-      }
-      return arg[1];
-    });
-  return hasSource ? warpJsonParse : JSONbig.parse;
-}
-const jsonParse = getJsonParse();
+// function getJsonParse() {
+//   let hasSource = false;
+//   const jsonStr = `{"myBigInt":6028792033986383748 }`;
+//   JSON.parse(jsonStr, (...arg) => {
+//     if (get(arg, "2")) hasSource = true;
+//     return arg[1];
+//   });
+//   const warpJsonParse = (jsonStr) =>
+//     JSON.parse(jsonStr, (...arg) => {
+//       if (typeof arg[1] === "number" && Number.isInteger(arg[1]) && !Number.isSafeInteger(arg[1])) {
+//         return new BigNumber(get(arg, "2.source"));
+//       }
+//       return arg[1];
+//     });
+//   return hasSource ? warpJsonParse : JSONbig.parse;
+// }
+const jsonParse = JSONbig.parse;
 
 const formatContentType = function (contentType, data) {
   const map = {
