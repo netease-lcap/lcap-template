dcc515daf84f73030e144ee92cc6873e196423c8
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2025-04-29 10:00:22 +0800

    fix: 调整源码init导入顺序 (#18) (#19)
    * fix: 修复$mixins找不到问题
    
    * fix: 同步调整微前端文件
    
    Co-authored-by: 御风 <18012261618@126.com>

--- packages/vue2/source/mobile/icestark/main.js
+++ packages/vue2/source/mobile/icestark/main.js
@@ -1,8 +1,8 @@
+import cloudAdminDesigner from "./init";
 import metaData from "./metaData.js";
 import platformConfig from "./platform.config.json";
 import { routes } from "./router/routes";
 import "./library";
-import cloudAdminDesigner from "./init";
 
 window.ICESTARK = window.ICESTARK || {};
 if (!window.ICESTARK.root) {
