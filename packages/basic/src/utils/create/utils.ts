64a66654a775e21ff3fede2675684cf8cfe985bf
刘宇阳 <myronliu347@gmail.com>
2025-05-22 15:49:49 +0800

    fix: stringify 循环引用使用标准库实现

--- packages/basic/src/utils/create/utils.ts
+++ packages/basic/src/utils/create/utils.ts
@@ -1,3 +1,5 @@
+import safeJSONStringify from 'safe-json-stringify';
+
 type Nil = null | undefined;
 /**
  * 若有定义errorMessage为string，且data中包含errorMsg字段，则直接写入字段尝试替换错误信息
@@ -8,8 +10,8 @@ export function overwriteErrorMsgFieldIfSpecified(
   data: (Record<string, unknown> & { errorMsg?: string | Nil }) | Nil,
   errorMessage: string | Nil,
 ) {
-  if (typeof errorMessage === "string" && data) {
-    if ("errorMsg" in data) {
+  if (typeof errorMessage === 'string' && data) {
+    if ('errorMsg' in data) {
       data.errorMsg = errorMessage;
     }
   }
@@ -18,26 +20,19 @@ export function overwriteErrorMsgFieldIfSpecified(
 /**
  * stringfy 过滤掉 function 和循环引用
  * */
-export function stringifyWithLoopProtection(obj, replacer?, space?) {
-  const seen = new WeakSet();
+export function stringifyWithLoopProtection(obj: any, replacer?: any, space?: any) {
   let hasCircleProp = false;
-  const result = JSON.stringify(
+
+  const result = safeJSONStringify(
     obj,
-    function (key, value) {
-      if (typeof value === "object" && value !== null) {
-        if (seen.has(value)) {
-          hasCircleProp = true;
-          // 如果已经见过该对象，则返回undefined;
-          return undefined;
-        }
-        seen.add(value);
+    (key, value) => {
+      if (value === '[Circular]') {
+        // 循环引用
+        hasCircleProp = true;
+        return undefined;
       }
-      // 使用用户提供的replacer函数（如果有的话）
-      if (replacer) {
-        return replacer(key, value);
-      } else {
-        return value;
-      }
+
+      return replacer(key, value);
     },
     space,
   );
