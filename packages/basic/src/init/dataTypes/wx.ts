e6b984a2a0896a16ab685c53a9a05a6de239c0f5
御风 <18012261618@126.com>
2025-05-23 18:15:30 +0800

    fix: 修复是否在lcap套壳微信小程序判断

--- packages/basic/src/init/dataTypes/wx.ts
+++ packages/basic/src/init/dataTypes/wx.ts
@@ -4,13 +4,14 @@
 const getUrl = (url) => (url.startsWith("http") ? url : "http://" + url);
 
 /* 小程序环境 */
-export const isMiniApp = (() => {
-  return localStorage.getItem("_wx_is_mini") || window.__wxjs_environment === "miniprogram";
-})();
+export const isLcapMiniApp = () => {
+  // 这样判断的原因：有用户希望在他们自己的小程序内嵌入webview，所以不能通过__wxjs_environment来判断
+  return localStorage.getItem("_wx_is_mini") === "1";
+};
 
 /* 路由跳转 */
 export const navigateTo = ({ url }) => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const origin = location.origin;
   const detailUrl = encodeURIComponent(`${origin}${url}`);
   const miniUrl = `/pages/index/index?detailUrl=${detailUrl}`;
@@ -18,13 +19,13 @@ export const navigateTo = ({ url }) => {
 };
 
 export const navigateBack = ({ delta = 1 }) => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
 
   window.wx.miniProgram.navigateBack({ delta });
 };
 
 export const redirectTo = ({ url }) => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const origin = location.origin;
   const detailUrl = encodeURIComponent(`${origin}${url}`);
   const miniUrl = `/pages/index/index?detailUrl=${detailUrl}`;
@@ -33,28 +34,28 @@ export const redirectTo = ({ url }) => {
 
 /* 跳转到头像昵称页面*/
 export const navigateToUserInfoPage = () => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const uri = location.href;
   window.wx.miniProgram.navigateTo({ url: `/pages/userinfo/index?redirect_uri=${uri}` });
 };
 
 /* 跳转到手机号页面*/
 export const navigateToUserPhonePage = () => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const uri = location.href;
   window.wx.miniProgram.navigateTo({ url: `/pages/userphone/index?redirect_uri=${uri}` });
 };
 
 /* 跳转到扫一扫页面*/
 export const navigateScanCodePage = () => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const uri = location.href;
   window.wx.miniProgram.navigateTo({ url: `/pages/scancode/index?redirect_uri=${uri}` });
 };
 
 /* 跳转到定位页面*/
 export const navigateLocationPage = () => {
-  if (!isMiniApp) return;
+  if (!isLcapMiniApp()) return;
   const uri = location.href;
   window.wx.miniProgram.navigateTo({ url: `/pages/getlocation/index?redirect_uri=${uri}` });
 };
