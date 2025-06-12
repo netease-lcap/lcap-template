b3ed9d2833c77a2848a87826fc7c2229cc5144cb
Sg <suica@users.noreply.github.com>
2025-06-12 15:00:19 +0800

    fix

--- packages/basic/src/init/dataTypes/tools.ts
+++ packages/basic/src/init/dataTypes/tools.ts
@@ -360,8 +360,8 @@ function exactMatchShapeAgainstDef(value, def: any): boolean {
   function isMatchForPrimitive(value, ty) {
     const valueTypeStr = Object.prototype.toString.call(value);
     // 检查enum类型
-    if (ty.typeKind === "primitive" && ty.concept === "Enum") {
-      return valueTypeStr === "[object String]";
+    if (ty.typeKind === 'primitive') {
+      return valueTypeStr === '[object String]';
     }
     if (ty.typeKind !== "primitive") {
       return false;
@@ -379,7 +379,9 @@ function exactMatchShapeAgainstDef(value, def: any): boolean {
   if (value === null) {
     return true;
   }
-  if (def.typeKind === "primitive") {
+  if (def.concept === "Enum") {
+    return def.enumItems?.some((item) => item.value === value);
+  } else if (def.typeKind === "primitive") {
     return isMatchForPrimitive(value, def);
   } else if (def.typeKind === "union") {
     for (const ty of def.typeArguments) {
