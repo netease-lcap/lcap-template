3da390aa50a17eb484ab6275d4451a7c912aa56d
御风 <18012261618@126.com>
2025-06-12 15:13:34 +0800

    feat: 添加 JSON 反序列化方法 (#46)

---
+++ packages/basic/tests/init/utils/deserializer.spec.js
@@ -0,0 +1,9 @@
+const utils = global.sdkUtils;
+
+describe('反序列化函数', () => {
+  test('JSON 反序列化兼容性测试，空值', () => {
+    expect(utils.JsonDeserialize('')).toBe(undefined);
+    expect(utils.JsonDeserialize(null)).toBe(null);
+    expect(utils.JsonDeserialize(undefined)).toBe(undefined);
+  });
+});
