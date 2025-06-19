5409f3b5730a49482e576c5bf20952c5b3b75868
御风 <18012261618@126.com>
2025-06-19 10:14:19 +0800

    Feature/vue3 i18n (#73)
    * feat: 添加vue3国际化支持
    
    * fix: 添加transformKeys

--- packages/vue3/source/pc/src/App.vue
+++ packages/vue3/source/pc/src/App.vue
@@ -8,7 +8,13 @@
 
 <script setup>
 import VErrorBoundary from './VErrorBoundary.vue';
-// TODO: 约定一种通用方式
-const locale = window.$libraries?.ElementPlus?.locale?.zhCn;
+import { getUserLanguage } from './i18n';
 
+const { appConfig } = window.appInfo;
+const { i18nInfo } = appConfig;
+// 获取当前语言
+const lang = getUserLanguage(appConfig, i18nInfo.messages);
+// 拿到messages
+const transformKeys = window.lcapStandardUI?.transformKeys || ((obj) => obj);
+const locale = i18nInfo.enabled ? transformKeys(i18nInfo.messages[lang]) : undefined;
 </script>
