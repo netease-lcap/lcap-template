4fc29be6a81dcf10549925167a134488343b44a2
Baymax808 <grp.lcap.fe.bot@corp.netease.com>
2025-07-04 17:12:40 +0800

    Task-3164046900461568: 导出源码规范
    * fix: 修复代码格式问题
    
    * fix(vue2): html format
    
    * chore: 源码调整
    
    * chore: eslint rule
    
    * fix: 更新viewport设置并调整样式格式
    
    ---------
    
    Co-authored-by: 御风 <18012261618@126.com>
    Co-authored-by: mcuking <tangjianghong@163.com>

--- packages/vue2/source/pc/eslint-config/plugins/lcap.js
+++ packages/vue2/source/pc/eslint-config/plugins/lcap.js
@@ -151,5 +151,46 @@ module.exports = {
             };
         },
     },
+    // 不必要的字符串模板使用
+    'no-unnecessary-template-literals': {
+      meta: {
+        type: "suggestion",
+        docs: {
+          description: "禁止不必要的模板字符串",
+          category: "Best Practices",
+        },
+        fixable: "code",
+        schema: [], // 不需要配置参数
+      },
+      create(context) {
+        return {
+          TemplateLiteral(node) {
+            if (node.expressions.length) {
+              // 如果有表达式，则不是不必要的模板字符串
+              return;
+            }
+
+            if (node.quasis.length > 1) {
+              // 如果有多个模板片段，则不是不必要的模板字符串
+              return;
+            }
+
+            const text = node.quasis[0].value.cooked || node.quasis[0].value.raw;
+            if (text.includes("\n") || text.includes("\r")) {
+              // 如果文本包含换行符，则不是不必要的模板字符串
+              return;
+            }
+
+            context.report({
+              node,
+              message: "不必要的模板字符串，直接使用字符串即可。",
+              fix(fixer) {
+                return fixer.replaceText(node, `'${text}'`); // 替换为单引号字符串
+              }
+            });
+          }
+        };
+      }
+    }
   }
 };
