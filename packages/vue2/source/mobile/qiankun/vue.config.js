c629daa2e632185907622f1bf17279d5cc6355e0
御风 <18012261618@126.com>
2025-07-10 17:23:15 +0800

    Task-3168427535600384: Vue2  publicPath

--- packages/vue2/source/mobile/qiankun/vue.config.js
+++ packages/vue2/source/mobile/qiankun/vue.config.js
@@ -1,7 +1,25 @@
 const path = require("path");
 const { name } = require("./package");
 
+/**
+ * publicPath 配置
+ * https://cli.vuejs.org/zh/config/#publicpath
+ */
+// 默认
+let publicPath;
+// build模式下
+if (process.env.NODE_ENV === "production") {
+  /**
+   * 使用LCAP平台的前缀（统一前缀+端路径）
+   * 
+   * 其他说明：
+   * 如果部署后资源访问异常，可尝试设置为空字符串 ('') 或是相对路径 ('./')，这样所有的资源都会被链接为相对路径
+   */
+  publicPath = '';
+}
+
 module.exports = {
+  publicPath,
   configureWebpack(config) {
     if (process.env.NODE_ENV === "production") {
       config.devtool = false;
