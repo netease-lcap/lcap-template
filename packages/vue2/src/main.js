577bcf97e5f1035ab94384390a1c455446bc3c34
御风 <18012261618@126.com>
2025-08-07 16:39:25 +0800

    fix: 恢复导入 library

--- packages/vue2/src/main.js
+++ packages/vue2/src/main.js
@@ -4,6 +4,8 @@ import metaData from "./metaData";
 import platformConfig from "./platform.config.json";
 import { routes } from "./router/routes";
 
+import "./library";
+
 import "@/assets/css/index.css";
 
 cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);
