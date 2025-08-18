5ebb092bd6df12204a255b39c4336ed3a63e614e
御风 <18012261618@126.com>
2025-08-18 17:43:44 +0800

    fix: 修复GlobalFn的Vue构造函数判断逻辑；更新rspack配置以禁用Vue生产环境的开发工具

--- packages/basic/src/global.ts
+++ packages/basic/src/global.ts
@@ -1,8 +1,8 @@
-import _set from "lodash/set";
+import _set from 'lodash/set';
 
 // 用来mock Vue的构造函数
 // @ts-ignore
-const GlobalFn = window.Vue || function MockVue() {};
+const GlobalFn = typeof window.Vue === 'function' ? window.Vue : function MockVue() {};
 
 export default GlobalFn;
 
