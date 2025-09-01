a85a884736db82e1f1160edcea253a44a510829f
御风 <18012261618@126.com>
2025-09-01 14:46:05 +0800

    Revert "Vue3支持umd入口文件 (#153)"
    This reverts commit a2f71e0d95f10e5827174da793831d4f7547061a.

--- packages/vue3/source/pc/src/common/plugins/logic/index.js
+++ packages/vue3/source/pc/src/common/plugins/logic/index.js
@@ -1,16 +1,7 @@
 import { initLogic } from '@lcap/basic-template';
-import { initLogicOverwrite } from './overwrite';
 
 export default {
   install(vm, options = {}) {
-    const { naslAPP } = options;
-
-    // 判断如果有 naslApp，就使用 overwrite 逻辑
-    if (naslAPP) {
-      return initLogicOverwrite(options);
-    }
-
-    // 否则使用原来的逻辑
     initLogic(options);
   },
 };
