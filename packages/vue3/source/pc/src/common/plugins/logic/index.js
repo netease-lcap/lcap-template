0b7f0e20b6a92da851575fb3a6dbfea327ccc4df
御风 <18012261618@126.com>
2025-09-03 11:36:29 +0800

    feat(vue3): init入口函数支持umd格式 (#155)
    * feat: Vue3 init入口函数支持umd格式
    
    * fix: 修复GlobalFn的Vue构造函数判断逻辑；更新rspack配置以禁用Vue生产环境的开发工具
    
    * feat: 更新rspack配置以支持vue-router，增强模块导出功能；添加hooks和全局变量到初始化文件
    
    * feat: adapte creator
    
    * fix: 更新rspack配置
    
    * refactor: 移除逻辑覆盖功能，简化插件安装逻辑
    
    * feat: 添加全局hooks和变量初始化
    
    ---------
    
    Co-authored-by: liwenyue <limoon7@163.com>

--- packages/vue3/source/pc/src/common/plugins/logic/index.js
+++ packages/vue3/source/pc/src/common/plugins/logic/index.js
@@ -1,7 +1,13 @@
-import { initLogic } from '@lcap/basic-template';
+import * as Basic from '@lcap/basic-template'
 
 export default {
   install(vm, options = {}) {
-    initLogic(options);
+    // 使用 override 逻辑
+    if (options?.override?.enabled) {
+      return options.override.initLogic(options, Basic);
+    }
+
+    // 否则使用原来的逻辑
+    Basic.initLogic(options);
   },
 };
--- packages/vue3/source/pc/src/hooks/index.js
+++ packages/vue3/source/pc/src/hooks/index.js
@@ -4,3 +4,4 @@ export * from './useDataSourceUtils';
 export * from './useWatchProps';
 export * from './useRefs';
 export * from './useElementStateAndProp';
+
