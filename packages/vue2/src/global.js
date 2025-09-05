363fd0a1dc5e9aea5f012b46a89e1b97a5a30688
御风 <18012261618@126.com>
2025-09-05 16:09:11 +0800

    fix: 移除全局变量中的VueCompositionAPI引用

--- packages/vue2/src/global.js
+++ packages/vue2/src/global.js
@@ -3,7 +3,6 @@
  */
 
 import Vue from 'vue';
-import * as VueCompositionAPI from '@vue/composition-api';
 import { install } from '@vusion/utils';
 import { createService, request as LcapRequest } from '@/common';
 import mixins from '@/mixins';
@@ -28,7 +27,6 @@ window.$sleep = function () {
 };
 Vue.prototype.$sleep = window.$sleep;
 
-window.VueCompositionAPI = VueCompositionAPI;
 // 全局混入
 window.$mixins = mixins;
 
