d4b852b31e9997abf975ef5101c61c76cade8d21
御风 <18012261618@126.com>
2025-09-05 16:04:24 +0800

    Revert "feat: 添加VueCompositionAPI到全局变量  (#163)" (#168)
    This reverts commit 63c855ba95625411e887af29b7b332bf50a34709.

--- packages/vue2/src/global.js
+++ packages/vue2/src/global.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import * as VueCompositionAPI from '@vue/composition-api';
 import { install } from '@vusion/utils';
 
 import { createService } from '@/common';
@@ -18,8 +17,6 @@ window.$sleep = function () {
   });
 };
 Vue.prototype.$sleep = window.$sleep;
-
-window.VueCompositionAPI = VueCompositionAPI;
 
 // 全局混入
 window.$mixins = mixins;
