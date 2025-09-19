9ee9fdf25c99b3ea8bd18d78edd7561c0580f348
mcuking <tangjianghong@163.com>
2025-09-19 16:27:10 +0800

    feat: 支持设置路由模式

--- packages/vue3/source/src/common/router/init.js
+++ packages/vue3/source/src/common/router/init.js
@@ -1,10 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createMemoryHistory, createWebHashHistory} from 'vue-router';
 
 export function createRouterInstance(routes) {
+  const fnMap = {
+    history: createWebHistory,
+    memory: createMemoryHistory,
+    hash: createWebHashHistory,
+  }
+  const createHistory = fnMap[window.LcapVueRouterConfig?.mode || 'history']
   const router = createRouter({
-    history: createWebHistory(window.LcapMicro?.routePrefix),
+    history: createHistory(window.LcapMicro?.routePrefix),
     routes,
   });
+
+  if (window.LcapVueRouterConfig?.initRoute) {
+    router.replace(window.LcapVueRouterConfig.initRoute);
+  }
 
   return router;
 }
