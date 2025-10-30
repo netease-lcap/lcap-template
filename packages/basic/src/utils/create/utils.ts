128e10dc1a5a1bbc7d3a2f5e766d89b83bc72d4c
御风 <18012261618@126.com>
2025-10-30 11:29:30 +0800

    fix: 添加 normalizeHeaderName 函数并在 defaultTransform 中调用以标准化请求头

--- packages/basic/src/utils/create/utils.ts
+++ packages/basic/src/utils/create/utils.ts
@@ -228,6 +228,33 @@ export function isURLSearchParams(val) {
   return typeof URLSearchParams !== 'undefined' && val instanceof URLSearchParams;
 }
 
+export function forEach(obj, fn) {
+  // Don't bother if no value provided
+  if (obj === null || typeof obj === 'undefined') {
+    return;
+  }
+
+  // Force an array if not already something iterable
+  if (typeof obj !== 'object') {
+    /*eslint no-param-reassign:0*/
+    obj = [obj];
+  }
+
+  if (isArray(obj)) {
+    // Iterate over array values
+    for (var i = 0, l = obj.length; i < l; i++) {
+      fn.call(null, obj[i], i, obj);
+    }
+  } else {
+    // Iterate over object keys
+    for (var key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        fn.call(null, obj[key], key, obj);
+      }
+    }
+  }
+}
+
 export const formatContentType = function (contentType, data) {
   const map = {
     'application/x-www-form-urlencoded'(data) {
@@ -236,3 +263,12 @@ export const formatContentType = function (contentType, data) {
   };
   return map[contentType] ? map[contentType](data) : data;
 };
+
+export function normalizeHeaderName(headers, normalizedName) {
+  forEach(headers, function processHeader(value, name) {
+    if (name !== normalizedName && name.toUpperCase() === normalizedName.toUpperCase()) {
+      headers[normalizedName] = value;
+      delete headers[name];
+    }
+  });
+}
