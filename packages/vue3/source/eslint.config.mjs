0f5dbb0b77f9790dd7ef13a9af74a132dd259caa
御风 <18012261618@126.com>
2025-11-12 15:24:48 +0800

    fix: 更新忽略文件列表，将 vite.config.ts 替换为 rspack.config.js

--- packages/vue3/source/eslint.config.mjs
+++ packages/vue3/source/eslint.config.mjs
@@ -24,7 +24,7 @@ export default [
       '**/router/**',
       'src/platform.config.json',
       'src/metaData.js',
-      'vite.config.ts',
+      'rspack.config.js',
       'eslint.config.mjs',
     ],
   },
