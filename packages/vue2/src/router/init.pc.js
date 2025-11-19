7a826230ce42247d648735d965b2947b47778b09
御风 <18012261618@126.com>
2025-11-19 12:42:15 +0800

    fix: 修复路由重复base问题

--- packages/vue2/src/router/init.pc.js
+++ packages/vue2/src/router/init.pc.js
@@ -1,7 +1,7 @@
 export function createRouter({ routes, VueRouter }) {
   return new VueRouter({
-    mode: "history",
-    base: window.LcapMicro?.routePrefix || process.env.BASE_URL,
+    mode: 'history',
+    base: window.LcapMicro?.routePrefix,
     routes,
   });
 }
