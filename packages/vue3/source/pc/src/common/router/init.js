e5b27ce5eef564b67d652890680425ac69958204
御风 <18012261618@126.com>
2025-12-08 14:02:16 +0800

    fix: 修复路由重复base问题 (#273)

--- packages/vue3/source/pc/src/common/router/init.js
+++ packages/vue3/source/pc/src/common/router/init.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export function createRouterInstance(routes) {
   const router = createRouter({
-    history: createWebHistory(window.LcapMicro?.routePrefix || import.meta.env.BASE_URL),
+    history: createWebHistory(window.LcapMicro?.routePrefix),
     routes,
   });
 
