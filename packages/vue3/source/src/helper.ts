37a5d7553094e0061b481bd29128a423dd3ce35d
御风 <18012261618@126.com>
2025-12-25 18:58:18 +0800

    feat: 优化权限指令的实例引用，简化条件判断；移除无效的 TypeScript 注释

--- packages/vue3/source/src/helper.ts
+++ packages/vue3/source/src/helper.ts
@@ -11,18 +11,14 @@ export async function renderApp() {
   platformConfig.appConfig.i18nInfo = i18nInfo;
 
   app = await cloudAdminDesigner.init(platformConfig?.appConfig, platformConfig, routes, metaData);
-  // @ts-expect-error
   window.createLcapApp = renderApp;
-  // @ts-expect-error
   window.appVM = {
     $destroy: unmountApp,
   };
 }
 
 function unmountApp() {
-  // @ts-expect-error
   window.appVM = null;
-  // @ts-expect-error
   window.createLcapApp = null;
   app?.unmount();
   app = null;
