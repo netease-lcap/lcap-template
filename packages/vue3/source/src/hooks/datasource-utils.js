1945dd95715637273d5d94b4198233cbd2fdf30e
御风 <18012261618@126.com>
2026-02-25 19:10:15 +0800

    fix(vue3): 移除不再使用的dependencies指令，并更新数据源工具函数的缓存逻辑

--- packages/vue3/source/src/hooks/datasource-utils.js
+++ packages/vue3/source/src/hooks/datasource-utils.js
@@ -5,41 +5,41 @@ export function useDataSourceUtils() {
 
   function __isShallowEqualArray(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
-    return arr1.every((current, i) => {
-      const other = arr2[i];
-      if (typeof current !== typeof other) return false;
-      if (typeof current === 'string') return current === other;
-      if (typeof current === 'object') {
-        return _isEqual(current, other);
+    return arr1.every((a, idx) => {
+      const b = arr2[idx];
+      if (typeof a !== typeof b) return false;
+      if (typeof a === 'string') return a === b;
+      if (typeof a === 'object') {
+        return _isEqual(a, b);
       }
       return false;
     });
   }
 
-  function __getDataSourceCacheFn(methodName, currentArray = []) {
-    if (!currentArray.length) {
-      return cache.get(methodName);
+  function __getDataSourceCacheFn(key, deps = []) {
+    if (!deps.length) {
+      return cache.get(key);
     }
-    for (let [key, value] of cache.entries()) {
-      if (__isShallowEqualArray(key, [methodName, ...currentArray])) {
+    for (let [cacheKey, value] of cache.entries()) {
+      if (__isShallowEqualArray(cacheKey, [key, ...deps])) {
         return value;
       }
     }
   }
 
-  function __setDataSourceCacheFn(methodName, currentArray = [], fn) {
-    if (!currentArray.length) {
-      cache.set(methodName, fn);
+  function __setDataSourceCacheFn(key, deps = [], fn) {
+    if (!deps.length) {
+      cache.set(key, fn);
       return;
     }
-    cache.set([methodName, ...currentArray], fn);
+    cache.set([key, ...deps], fn);
   }
 
-  function __getOrCreateDataSource(methodName, currentArray = [], newFn) {
-    let fn = __getDataSourceCacheFn(methodName, currentArray);
+  function __getOrCreateDataSource(key, deps = [], newFn) {
+    let fn = __getDataSourceCacheFn(key, deps);
     if (!fn) {
       fn = newFn;
-      __setDataSourceCacheFn(methodName, currentArray, fn);
+      __setDataSourceCacheFn(key, deps, fn);
     }
     return fn;
   }
