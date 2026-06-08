0b3fa739b1d485bd9bdc5fcd069dbd72d7517cd8
derdersama <133649135+derdersama@users.noreply.github.com>
2026-06-08 15:35:29 +0800

    feat(vue2): 源码工程添加默认chunk分包规则 (#368)

--- packages/vue2/source/pc/vue.config.js
+++ packages/vue2/source/pc/vue.config.js
@@ -4,7 +4,35 @@ const path = require("path");
 module.exports = {
   configureWebpack(config) {
     if (process.env.NODE_ENV === "production") {
+      // 关闭 sourcemap，减少 map 文件体积
       config.devtool = false;
+
+      // 代码分割：将 node_modules 按包名拆分（利于缓存）或合并为一个 chunk
+      // 若服务器支持 HTTP/2，按包拆分更佳；否则可合并以减少请求数
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: 6,
+        maxAsyncRequests: 10,
+        cacheGroups: {
+          vue: {
+            test: /[\\/]node_modules[\\/](@?vue|pinia)/,
+            name: 'chunk-vue',
+            priority: 40,
+            enforce: true,
+          },
+          lcap: {
+            test: /[\\/]node_modules[\\/](@?lcap)/,
+            name: 'chunk-lcap',
+            priority: 30,
+            enforce: true,
+          },
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'chunk-vendors',
+            priority: 20,
+          },
+        },
+      };
     }
 
     /// cloud-ui-alias-start
@@ -13,16 +41,26 @@ module.exports = {
     config.resolve.alias["cloud-ui.vusion"] = "@lcap/pc-ui";
     /// cloud-ui-alias-end
   },
+
   chainWebpack(config) {
-    // 构建产物中删除console相关代码
-    // config.optimization.minimizer('terser')
-    //     .tap((args) => {
-    //         args[0].terserOptions.compress.drop_console = ['info', 'log', 'warn'];
-    //         return args;
-    //     });
+    // 删除 console 语句
+    config.optimization.minimizer('terser')
+      .tap((args) => {
+        args[0].terserOptions = args[0].terserOptions || {};
+        args[0].terserOptions.compress = args[0].terserOptions.compress || {};
+        args[0].terserOptions.compress.drop_console = ['info', 'log', 'warn'];
+        return args;
+      });
   },
-  lintOnSave: false,
+
+  // 注意：runtimeCompiler: true 会启用包含模板编译器的构建（runtime + compiler），产物体积更大；仅在需要运行时编译模板时开启
   runtimeCompiler: true,
+
+  // 关闭生产环境的 sourcemap（以免生成 .js.map 文件）
+  productionSourceMap: false,
+
+  lintOnSave: false,
+
   devServer: {
     compress: true,
     port: 8810,
@@ -48,7 +86,7 @@ module.exports = {
         autoRewrite: true,
       },
       "^/gw/": {
-        target: `http://localhost:8080`,
+        target: "http://localhost:8080",
         changeOrigin: true,
         autoRewrite: true,
       },
